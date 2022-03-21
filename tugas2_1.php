eda2077516401c166aed6dc69d93917dc161a2fe
Yehezkiel Reynaldi Abraham <yehezkielreynaldi01@gmail.com>
2022-03-21 19:57:13 +0700

    Tugas 2 PHP
    Tugas ini saya kirimkan untuk Niomic,Terimakasih.

---
+++ tugas2_1.php
@@ -0,0 +1,13 @@
+<?php
+$hari_1 = 36;
+$hari_x = 2;
+$pekerja_1 = 12;
+$pekerja_2 = 24;
+$hitung1 = "$hari_1 Hari = $pekerja_1 Pekerja<br>";
+$hitung2 =  "X hari = $pekerja_2 Pekerja";
+$hitung3 = $hari_1 / $hari_x;
+$jawaban = 18;
+echo "$hitung1";
+echo "$hitung2<br>";
+echo "X = $hari_1 / $hari_x<Br>";
+echo "Jawaban : X = $jawaban hari";
---
+++ tugas2_2.php
@@ -0,0 +1,10 @@
+<?php
+$jual = 80000;
+$beli = 50000;
+$untung = 30000;
+$keuntungan = $untung / $beli * 100;
+
+echo "Jual = $jual<br>";
+echo "Beli = $beli<br>";
+echo "Keuntungan = $untung<br>";
+echo "Jawaban = $keuntungan%";
